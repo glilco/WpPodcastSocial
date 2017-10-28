1cd61c8b345896342c94dbe4ca12661e8531ab4f
Murilo Ferraz <glilco@gmail.com>
2017-10-28 12:23:12 -0200

    Fixed issue with description
    * Fixed getting description from itunes:summary
    * Fixed Author showing

--- plugins/bp-podcasts/bp-after-buddypress.php
+++ plugins/bp-podcasts/bp-after-buddypress.php
@@ -1,6 +1,6 @@
 <?php
 global $debug_text;
-$debug_text = true;
+$debug_text = false;
 
 if(!bp_is_active('groups') && !is_admin()) {
   die('Needs BuddyPress groups to work');
@@ -144,7 +144,6 @@ function create_podcast_feed($feed_url) {
       
       if(!$podcast_data || !isset($podcast_data['TITLE']) || !isset($podcast_data['DESCRIPTION']) || !isset($podcast_data['LINK'])) {
           if($debug_text)echo '<br /> Não foi possível obter dados do podcast ';
-          die();
           return false;
       }
       
